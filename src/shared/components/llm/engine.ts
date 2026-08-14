af9a40fde5ca24bd7c601e9adb0bbd1c89348d95
Bob Park <hwpark11@kakao.com>
2026-08-15 01:08:27 +0900

    feat: engine 정의

--- src/shared/components/llm/engine.ts
+++ src/shared/components/llm/engine.ts
@@ -2,7 +2,46 @@ import { CreateWebWorkerMLCEngine, WebWorkerMLCEngine } from '@mlc-ai/web-llm';
 
 let enginePromise: Promise<WebWorkerMLCEngine> | null = null;
 
-export const DEFAULT_MODEL_ID = 'Qwen2.5-1.5B-Instruct-q4f16_1-MLC';
+export type EngineModel = {
+  id: string;
+  displayName: string;
+  size: number;
+};
+
+export const supportEngines = [
+  {
+    id: 'Qwen2.5-1.5B-Instruct-q4f16_1-MLC',
+    displayName: 'Qwen2.5 1.5B',
+    size: 4,
+  },
+  {
+    id: 'Qwen2.5-3B-Instruct-q4f16_1-MLC',
+    displayName: 'Qwen2.5 3B',
+    size: 8,
+  },
+  {
+    id: 'Qwen2.5-7B-Instruct-q4f16_1-MLC',
+    displayName: 'Qwen2.5 7B',
+    size: 16,
+  },
+  {
+    id: 'Qwen3-0.6B-q4f16_1-MLC',
+    displayName: 'Qwen3 0.6B',
+    size: 4,
+  },
+  {
+    id: 'Qwen3-4B-q4f32_1-MLC',
+    displayName: 'Qwen3 4B',
+    size: 16,
+  },
+  {
+    id: 'Qwen3-8B-q4f32_1-MLC',
+    displayName: 'Qwen3 8B',
+    size: 16,
+  },
+];
+
+export const DEFAULT_MODEL_ID = 'Qwen2.5-3B-Instruct-q4f16_1-MLC';
 
 type EngineProps = {
   modelId?: string;
